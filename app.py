45d53d5dc8d2f01a349c8cebc952e15ea99ee76e
norkina712-eng <norkina712@gmail.com>
2026-05-18 14:08:44 +0300

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,52 @@
 from flask import Flask, request
+from openai import OpenAI
+import os
 
 app = Flask(__name__)
 
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
+
     data = request.json
 
+    text = data.get("message", {}).get("text", "")
+
     print("NEW COMMENT:")
-    print(data)
+    print(text)
+
+    if text:
+
+        prompt = f"""
+        Проанализируй комментарий пользователя.
+
+        Определи:
+        1. Тему обучения
+        2. Тип вопроса
+        3. Нужен ли менеджер
+        4. Температуру лида
+
+        Комментарий:
+        {text}
+        """
+
+        response = client.chat.completions.create(
+            model="gpt-4.1-mini",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        )
+
+        result = response.choices[0].message.content
+
+        print("AI ANALYSIS:")
+        print(result)
 
     return "ok"
 
